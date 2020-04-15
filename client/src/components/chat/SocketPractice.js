75178a1e5538644279c7e7fa7cdc782af85307d0
venkatesh sekaran <venkisekaran77@gmail.com>
2020-04-15 11:21:19 +0530

    third commit

--- client/src/components/chat/SocketPractice.js
+++ client/src/components/chat/SocketPractice.js
@@ -7,7 +7,7 @@ import Messages from "./Messages"
 import Sidebar from "./Sidebar"
 
 let socket
-const hostname = "localhost:5000"
+const hostname = "https://pure-brook-40724.herokuapp.com/"
 socket = io(hostname)
 
 const Socketpractice = ({ location }) => {
